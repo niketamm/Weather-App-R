eed7e08f27b1e44773e0af18a0d977528c4dc6d6
Nike <reintamm.nike@gmail.com>
2021-09-26 22:57:47 +0900

    missing line to make bootstrap added
    App.js did not have css of bootstrap link added, after adding bootstrap is ready for use

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
   Hello
+  <div className="btn btn-primary">Primary</div>
     </div>
   );
 }
