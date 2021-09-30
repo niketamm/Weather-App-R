5f54dd7cfb3fd8fe5254813b24e6e49e44c2487a
Nike <reintamm.nike@gmail.com>
2021-09-30 21:20:42 +0900

    added footer

--- src/App.js
+++ src/App.js
@@ -6,33 +6,42 @@ import WeatherIcon from "./WeatherIcon";
 
 function App() {
   return (
+    <div className="Body">
     <div className="App">
-      <div className="Info">
-</div>
-<div className="Icon">
-   <WeatherIcon icon="CLEAR_DAY" color="#C7EEFF" size="150" />
-</div>
- <div className="Info">
-It is 25℃
-Tokyo, Japan
-</div>
-      <div className="Form">
-        <form>
+
+        <div className="Icon">
+          <WeatherIcon icon="CLEAR_DAY" color="#C7EEFF" size="150" />
+        </div>
+
+        <div className="Info">
+          It is 25℃
+          Tokyo, Japan
+        </div>
+
+        <div className="Form">
+          <form>
             <input type="text" placeholder="enter city and press enter"  className="search"/>
             <input type="submit" value="Search" className="SearchButton"/>
-        </form>
-      </div>
-      <div className="Info"> 
-      <ul>
-      <li>Last Updated</li>
-      <li>Humidity:</li>
-      <li>Wind:</li>
-      <li>Lowest:</li>
-      <li>Highest:</li>
-      </ul>
-      </div>
+         </form>
+        </div>
 
-  </div>
+
+        <div className="Info"> 
+         <ul>
+          <li>Last Updated</li>
+          <li>Humidity:</li>
+          <li>Wind:</li>
+          <li>Lowest:</li>
+          <li>Highest:</li>
+          </ul>
+        </div>
+    
+    </div>
+
+    <div>
+        Coded by Nike
+    </div>
+    </div>
   );
 }
 
