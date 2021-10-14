dd6777548d637cced7d64cac298c13b977fae944
Nike <reintamm.nike@gmail.com>
2021-10-14 20:24:59 +0900

    update forecast by search
    adding useEffect to debug forecast not refreshing

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import axios from "axios";
 import FormatForecastDate from "./FormatForecastDate";
 
@@ -7,6 +7,10 @@ export default function WeatherForecast(props){
     let [loaded, setLoaded]=useState(false);
     let [forecast, setForecast]=useState(null);
 
+    useEffect(()=>{
+       setLoaded(false);
+    }, [props.coordinates])
+
     function showForecast(response){
  setForecast(response.data.daily);
  setLoaded(true);
@@ -14,16 +18,19 @@ export default function WeatherForecast(props){
 
 if (loaded){
      return (<div className="row">
-            {forecast.map(function(dailyForecast, index){
-                if (index >3) {return (
-                <div className="col" key={index}>
-                <FormatForecastDate data={dailyForecast}/>
-                </div>  
-                )
+                {forecast.map(function (dailyForecast, index) {
+                if (index >3) {
+                    return (
+                    <div className="col" key={index}>
+                        <FormatForecastDate data={dailyForecast} />
+                    </div>  
+                    );
                 }
             })}
-                </div>   );
-}else{let apiKey=`8e829c86fcad97d52771623e3da2a60f`;
+                </div>   
+                );
+    }
+else {let apiKey=`8e829c86fcad97d52771623e3da2a60f`;
     let latitude=(props.coordinates.lat);
     let longitude=(props.coordinates.lon);
     let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
